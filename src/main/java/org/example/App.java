f3ec6a2c459f40933ecc22061e0525940c010049
Farhad Towfighian <fartow@gmail.com>
2022-10-10 17:28:52 +0200

    test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,5 +5,24 @@ public class App {
 
         System.out.println("Hello");
         System.out.println("Farhad!");
+        //end of exercise 1
+        //exercise 3 begin here! using diff. methods
+        int N1 = 23;
+        int N2 = 11;
+        int result = N1 + N2;
+        System.out.println("23 + 11 = " + result);
+        int number1 = 40;
+        int number2 = 24;
+        int result1 = number1 - number2;
+        System.out.println("40 - 24 = " + result1);
+        int number3 = number1 * 12;
+        System.out.println("40 * 12 = " + number3);
+        double result3 = number2 / 1.34;
+        System.out.println("24 / 1.34 = " + result3);
+        //end of exercise 3
+        //exercise 4 begin here!
+
+
+
     }
 }
